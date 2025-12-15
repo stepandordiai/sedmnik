a1bde38e67f77b5aea79bbe1798c5f870967ceca
Stepan Dordiai <stepandordiai@gmail.com>
2025-12-15 09:15:32 +0100

    ...

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -34,26 +34,103 @@ const dayNow = weekData[correctDate];
 const month = dateNow.getMonth();
 const date = dateNow.getDate();
 const monthNow = monthData[month];
+const yearNow = dateNow.getFullYear();
 
 const Home = () => {
-	const [usersData, setUsersData] = useState([]);
+	interface User {
+		date: string;
+		people: {
+			name: string;
+			notes: string;
+		}[];
+		updatedAt: string;
+	}
+
+	interface Person {
+		name: string;
+		notes: string;
+	}
+
+	interface InputData {
+		date: Date;
+		people: Person[];
+	}
+
+	const [userData, setUserData] = useState<User[] | null>([]);
+
+	const [loading, setLoading] = useState(false);
+
+	const [inputData, setInputData] = useState<InputData>({
+		date: new Date(),
+		people: [],
+	});
 
 	useEffect(() => {
 		const getUsersData = async () => {
+			setLoading(true);
 			try {
 				const response = await axios.get(
 					"https://weekly-planner-backend.onrender.com/api"
 				);
-				setUsersData(response.data);
+				setUserData(response.data);
 			} catch (error) {
 				console.log(error);
+			} finally {
+				setLoading(false);
 			}
 		};
 
 		getUsersData();
 	}, []);
 
-	console.log(usersData);
+	const updateUsersData = async (e: any) => {
+		e.preventDefault();
+		try {
+			await axios.put(
+				"https://weekly-planner-backend.onrender.com/api",
+				inputData
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const usersDayNow = userData?.find(
+		(users) => users.date.slice(0, 10) === `${yearNow}-${month + 1}-${date}`
+	);
+
+	useEffect(() => {
+		if (!usersData?.length) return;
+
+		const people = usersData.map((user) => {
+			const existingPerson = usersDayNow?.people.find(
+				(p) => p.name === user.name
+			);
+
+			return {
+				name: user.name,
+				notes: existingPerson?.notes || "",
+			};
+		});
+
+		setInputData({
+			date: usersDayNow?.date ? new Date(usersDayNow.date) : new Date(),
+			people,
+		});
+	}, [usersData, usersDayNow]);
+
+	// TODO: LEARN THIS
+	const handlePersonChange = (index: number, field: string, value: string) => {
+		const updatedPeople: any = [...inputData.people];
+		updatedPeople[index][field] = value;
+
+		setInputData({
+			...inputData,
+			people: updatedPeople,
+		});
+	};
+
+	if (loading || !userData) return;
 
 	return (
 		<main className="home">
@@ -62,18 +139,22 @@ const Home = () => {
 				{dayNow} {monthNow} {date}
 			</p>
 			<div className="home-users-container">
-				{usersData.map((user) => {
+				{usersData.map((user, i) => {
 					return (
 						<div className="home-user-container" key={user.id}>
 							<div style={{ display: "flex", justifyContent: "space-between" }}>
 								<p>{user.name}</p>
-								<p>13:47</p>
+								{/* <p>{userData?.updatedAt}</p> */}
 							</div>
-							<form action="">
+							<form action="" onSubmit={updateUsersData}>
 								{/* <input type="text" placeholder="text" /> */}
 								<textarea
+									value={inputData.people?.[i]?.notes ?? ""}
+									onChange={(e) =>
+										handlePersonChange(i, "notes", e.target.value)
+									}
 									className="textarea"
-									name=""
+									name={user.name}
 									id=""
 									rows={5}
 								></textarea>
