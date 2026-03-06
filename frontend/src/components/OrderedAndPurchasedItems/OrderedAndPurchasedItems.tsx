ee7594a5985b471912fa530c8f8d6ae1c0f9abe1
Stepan Dordiai <stepandordiai@gmail.com>
2026-03-06 13:09:59 +0100

    feat: update OrderedAndPurchasedItems with filter

--- frontend/src/components/OrderedAndPurchasedItems/OrderedAndPurchasedItems.tsx
+++ frontend/src/components/OrderedAndPurchasedItems/OrderedAndPurchasedItems.tsx
@@ -14,6 +14,34 @@ const emptyInput = () => ({
 	orderDate: "",
 });
 
+const ORDER_STATUS = [
+	{
+		id: 1,
+		label: "Nezvoleno",
+		className: "",
+	},
+	{
+		id: 2,
+		label: "S dopravou",
+		className: "input--green",
+	},
+	{
+		id: 3,
+		label: "K výzvednutí",
+		className: "input--orange",
+	},
+	{
+		id: 4,
+		label: "Poptáno",
+		className: "input--red",
+	},
+	{
+		id: 5,
+		label: "Dodáno",
+		className: "input--blue",
+	},
+];
+
 const OrderedAndPurchasedItems = ({ buildingId }) => {
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(false);
@@ -96,18 +124,38 @@ const OrderedAndPurchasedItems = ({ buildingId }) => {
 	const addEmptyInput = () =>
 		setOrderedItems((prev) => [...prev, emptyInput()]);
 
+	const [filter, setFilter] = useState("");
+
 	return (
 		<section className="section-table">
 			<div
 				style={{
 					display: "flex",
 					justifyContent: "space-between",
-					alignItems: "cnter",
+					alignItems: "center",
+					gap: 10,
+					flexWrap: "wrap",
+					margin: "5px 5px 0 5px",
 				}}
 			>
-				<h2 style={{ margin: "5px 0 0 5px ", fontWeight: 600 }}>
-					Objednané a dodané materiály
-				</h2>
+				<h2 style={{ fontWeight: 600 }}>Objednané a dodané materiály</h2>
+				<div
+					style={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						gap: 5,
+					}}
+				>
+					<label htmlFor="filter">Filter</label>
+					<input
+						onChange={(e) => setFilter(e.target.value)}
+						className="input filter"
+						id="filter"
+						type="text"
+						placeholder="Hledat materiály..."
+					/>
+				</div>
 			</div>
 			<table>
 				<thead>
@@ -118,83 +166,85 @@ const OrderedAndPurchasedItems = ({ buildingId }) => {
 					</tr>
 				</thead>
 				<tbody>
-					{orderedItems.map((orderedItem) => {
-						return (
-							<tr key={orderedItem.id}>
-								<td style={{ width: "100%" }}>
-									<AutoGrowTextArea
-										value={orderedItem.desc}
-										handleChange={(e) =>
-											handleOrderedItems(
-												orderedItem.id,
-												e.target.name,
-												e.target.value,
-											)
-										}
-										name="desc"
-										blur={saveOrderedItemsData}
-										disable={loading}
-									/>
-								</td>
-								<td>
-									<select
-										onChange={(e) => {
-											handleOrderedItems(
-												orderedItem.id,
-												e.target.name,
-												e.target.value,
-											);
-										}}
-										value={orderedItem.orderOption}
-										name="orderOption"
-										className={classNames("input", {
-											"input--green": orderedItem.orderOption === "S dopravou",
-											"input--orange":
-												orderedItem.orderOption === "K výzvednutí",
-											"input--red": orderedItem.orderOption === "Poptáno",
-											"input--blue": orderedItem.orderOption === "Dodáno",
-											"select--disabled": loading,
-										})}
-										onBlur={saveOrderedItemsData}
-									>
-										<option value="">Nezvoleno</option>
-										<option className="input--green" value="S dopravou">
-											S dopravou
-										</option>
-										<option className="input--orange" value="K výzvednutí">
-											K výzvednutí
-										</option>
-										<option className="input--red" value="Poptáno">
-											Poptáno
-										</option>
-										<option className="input--blue" value="Dodáno">
-											Dodáno
-										</option>
-									</select>
-								</td>
-								<td>
-									<input
-										onBlur={saveOrderedItemsData}
-										onChange={(e) =>
-											handleOrderedItems(
-												orderedItem.id,
-												e.target.name,
-												e.target.value,
-											)
-										}
-										value={orderedItem.orderDate}
-										name="orderDate"
-										style={{ width: "100%" }}
-										className={classNames("input", {
-											"input--disabled": loading,
-										})}
-										type="date"
-										disabled={loading}
-									/>
-								</td>
-							</tr>
-						);
-					})}
+					{orderedItems
+						.filter((item) =>
+							item.desc.toLowerCase().includes(filter.toLowerCase().trim()),
+						)
+						.map((orderedItem) => {
+							return (
+								<tr key={orderedItem.id}>
+									<td style={{ width: "100%" }}>
+										<AutoGrowTextArea
+											value={orderedItem.desc}
+											handleChange={(e) =>
+												handleOrderedItems(
+													orderedItem.id,
+													e.target.name,
+													e.target.value,
+												)
+											}
+											name="desc"
+											blur={saveOrderedItemsData}
+											disable={loading}
+										/>
+									</td>
+									<td>
+										<select
+											onChange={(e) => {
+												handleOrderedItems(
+													orderedItem.id,
+													e.target.name,
+													e.target.value,
+												);
+											}}
+											value={orderedItem.orderOption}
+											name="orderOption"
+											className={classNames("input", {
+												"input--green":
+													orderedItem.orderOption === "S dopravou",
+												"input--orange":
+													orderedItem.orderOption === "K výzvednutí",
+												"input--red": orderedItem.orderOption === "Poptáno",
+												"input--blue": orderedItem.orderOption === "Dodáno",
+												"select--disabled": loading,
+											})}
+											onBlur={saveOrderedItemsData}
+										>
+											{ORDER_STATUS.map((status) => {
+												return (
+													<option
+														className={status.className}
+														value={status.label}
+													>
+														{status.label}
+													</option>
+												);
+											})}
+										</select>
+									</td>
+									<td>
+										<input
+											onBlur={saveOrderedItemsData}
+											onChange={(e) =>
+												handleOrderedItems(
+													orderedItem.id,
+													e.target.name,
+													e.target.value,
+												)
+											}
+											value={orderedItem.orderDate}
+											name="orderDate"
+											style={{ width: "100%" }}
+											className={classNames("input", {
+												"input--disabled": loading,
+											})}
+											type="date"
+											disabled={loading}
+										/>
+									</td>
+								</tr>
+							);
+						})}
 				</tbody>
 			</table>
 			<button
